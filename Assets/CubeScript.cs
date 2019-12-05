bdb3f94a8b4c0fc624b6d6b6f1ff92335db63f47
chinen <croquette.picker@gmail.com>
2019-12-05 18:07:57 +0900

    chinen プレイヤーの向きが移動方向を向くように修正

--- Assets/CubeScript.cs
+++ Assets/CubeScript.cs
@@ -7,10 +7,17 @@ public class CubeScript : MonoBehaviourPunCallbacks
     public float jumpPower = 20.0f;
     public float buttobiPower = 50.0f;
     public Rigidbody rb;
+
+    // プレイヤーのポジション
+    private Vector3 Player_pos; 
+
     // Start is called before the first frame update
     void Start()
     {
         this.rb = this.GetComponent<Rigidbody>();
+
+        //最初の時点でのプレイヤーのポジションを取得
+        Player_pos = GetComponent<Transform>().position; 
     }
     // Update is called once per frame
     void Update()
@@ -40,6 +47,17 @@ public class CubeScript : MonoBehaviourPunCallbacks
         {
             rb.AddForce(0, jumpPower, 0);
         }
+
+        //プレイヤーがどの方向に進んでいるかがわかるように、初期位置と現在地の座標差分を取得
+        Vector3 diff = transform.position - Player_pos; 
+        Quaternion lockRotation = Quaternion.LookRotation(diff, Vector3.up);
+        if (diff.magnitude > 0.01f) //ベクトルの長さが0.01fより大きい場合にプレイヤーの向きを変える処理を入れる(0では入れないので）
+        {
+            lockRotation.x = 0;
+            lockRotation.z = 0;
+            transform.rotation = Quaternion.Lerp(transform.rotation, lockRotation, 0.1f);
+        }
+        Player_pos = transform.position;
     }
     private void OnCollisionEnter(Collision collision)
     {
