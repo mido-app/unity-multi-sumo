8597ec46031ee687568920673a47dbc492333900
ULDOMAIN\yutaka.omido <yutaka.omido@ulsystems.co.jp>
2019-12-05 17:51:13 +0900

    add deadzone script and material

---
+++ Assets/DeadZone.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+
+public class DeadZone : MonoBehaviour
+{
+    public Vector3 respawnPosition;
+    private BoxCollider collaider;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        this.collaider = this.GetComponent<BoxCollider>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.tag == "Player")
+        {
+            other.gameObject.GetComponent<Rigidbody>().velocity = Vector3.zero;
+            other.gameObject.transform.position = this.respawnPosition;
+        }
+    }
+}
