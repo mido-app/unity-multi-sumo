622b9b81cf78e3f844ab6c18360815df1b6db390
ULDOMAIN\yutaka.omido <yutaka.omido@ulsystems.co.jp>
2019-12-06 00:30:17 +0900

    add audio and se

---
+++ Assets/SEController.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+
+public class SEController : MonoBehaviour
+{
+    public AudioClip[] seClipList;
+    private AudioSource audioSource;
+    // Start is called before the first frame update
+    void Start()
+    {
+        this.audioSource = this.GetComponent<AudioSource>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    public void PlayRandomAttackSE()
+    {
+        this.audioSource.PlayOneShot(this.seClipList[Random.Range(0, this.seClipList.Length - 1)]);
+    }
+}
