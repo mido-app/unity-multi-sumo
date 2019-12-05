527aa295f106d298097baf922ef50f454f6de9ae
chinen <croquette.picker@gmail.com>
2019-12-05 14:54:57 +0900

    unity初期ファイル

---
+++ Assets/cubeForceScript.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class cubeForceScript : MonoBehaviour
+{
+    public float speed = 10.0f;
+    public Rigidbody rb;
+
+    void Start() 
+    {
+        rb = GetComponent<Rigidbody>();
+    }
+
+    void Update() 
+    {
+        float x =  Input.GetAxis("Horizontal") * speed;
+        float z = Input.GetAxis("Vertical") * speed;
+        rb.AddForce(x , 0 , z );
+    }
+}
---
+++ Assets/photonSampleCode.cs
@@ -0,0 +1,26 @@
+﻿using Photon.Pun;
+using Photon.Realtime;
+using UnityEngine;
+
+public class photonSampleCode : MonoBehaviourPunCallbacks
+{
+    // MonoBehaviourではなくMonoBehaviourPunCallbacksを継承して、Photonのコールバックを受け取れるようにする    
+    private void Start() {
+        // PhotonServerSettingsに設定した内容を使ってマスターサーバーへ接続する
+        PhotonNetwork.ConnectUsingSettings();
+    }
+
+    // マスターサーバーへの接続が成功した時に呼ばれるコールバック
+    public override void OnConnectedToMaster() {
+        // "room"という名前のルームに参加する（ルームが無ければ作成してから参加する）
+        PhotonNetwork.JoinOrCreateRoom("room", new RoomOptions(), TypedLobby.Default);
+    }
+
+    // マッチングが成功した時に呼ばれるコールバック
+    public override void OnJoinedRoom() {
+        // マッチング後、ランダムな位置に自分自身のネットワークオブジェクトを生成する
+        // Debug.log("successssssss!!!!");
+        var v = new Vector3(0,0.5f,0);
+        PhotonNetwork.Instantiate("Rikishi", v, Quaternion.identity);
+    }
+}
