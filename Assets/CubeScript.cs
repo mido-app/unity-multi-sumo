e54c1db043a99e6ad37e1f0fbd4bd2f9663d0b25
ULDOMAIN\yutaka.omido <yutaka.omido@ulsystems.co.jp>
2019-12-05 23:03:21 +0900

    fix respawn position

--- Assets/CubeScript.cs
+++ Assets/CubeScript.cs
@@ -6,10 +6,10 @@ public class CubeScript : MonoBehaviourPunCallbacks
     public float speed = 1.0f;
     public float jumpPower = 20.0f;
     public float buttobiPower = 50.0f;
-    public Rigidbody rb;
+    private Rigidbody rb;
 
     // プレイヤーのポジション
-    private Vector3 Player_pos; 
+    private Vector3 Player_pos;
 
     // Start is called before the first frame update
     void Start()
@@ -17,7 +17,7 @@ public class CubeScript : MonoBehaviourPunCallbacks
         this.rb = this.GetComponent<Rigidbody>();
 
         //最初の時点でのプレイヤーのポジションを取得
-        Player_pos = GetComponent<Transform>().position; 
+        Player_pos = GetComponent<Transform>().position;
     }
     // Update is called once per frame
     void Update()
@@ -49,7 +49,7 @@ public class CubeScript : MonoBehaviourPunCallbacks
         }
 
         //プレイヤーがどの方向に進んでいるかがわかるように、初期位置と現在地の座標差分を取得
-        Vector3 diff = transform.position - Player_pos; 
+        Vector3 diff = transform.position - Player_pos;
         Quaternion lockRotation = Quaternion.LookRotation(diff, Vector3.up);
         if (diff.magnitude > 0.01f) //ベクトルの長さが0.01fより大きい場合にプレイヤーの向きを変える処理を入れる(0では入れないので）
         {
--- Assets/photonSampleCode.cs
+++ Assets/photonSampleCode.cs
@@ -5,22 +5,25 @@ using UnityEngine;
 public class photonSampleCode : MonoBehaviourPunCallbacks
 {
     // MonoBehaviourではなくMonoBehaviourPunCallbacksを継承して、Photonのコールバックを受け取れるようにする    
-    private void Start() {
+    private void Start()
+    {
         // PhotonServerSettingsに設定した内容を使ってマスターサーバーへ接続する
         PhotonNetwork.ConnectUsingSettings();
     }
 
     // マスターサーバーへの接続が成功した時に呼ばれるコールバック
-    public override void OnConnectedToMaster() {
+    public override void OnConnectedToMaster()
+    {
         // "room"という名前のルームに参加する（ルームが無ければ作成してから参加する）
         PhotonNetwork.JoinOrCreateRoom("room", new RoomOptions(), TypedLobby.Default);
     }
 
     // マッチングが成功した時に呼ばれるコールバック
-    public override void OnJoinedRoom() {
+    public override void OnJoinedRoom()
+    {
         // マッチング後、ランダムな位置に自分自身のネットワークオブジェクトを生成する
         // Debug.log("successssssss!!!!");
-        var v = new Vector3(0,0.5f,0);
+        var v = new Vector3(Random.Range(-4.0f, 4.0f), 0.5f, Random.Range(-4.0f, 4.0f));
         PhotonNetwork.Instantiate("Rikishi", v, Quaternion.identity);
     }
 }
