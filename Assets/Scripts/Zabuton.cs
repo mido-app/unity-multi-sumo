247fee5db30fc054d0c143b502a4972c438045ce
ULDOMAIN\yutaka.omido <yutaka.omido@ulsystems.co.jp>
2019-12-06 11:57:49 +0900

    add zabuton and zabuton spawner

---
+++ Assets/Scripts/Zabuton.cs
@@ -0,0 +1,35 @@
+﻿using UnityEngine;
+
+public class Zabuton : MonoBehaviour
+{
+    public float moveSpeedMin;
+    public float moveSpeedMax;
+    public float rotetionSpeed = 10f;
+
+    public float _moveSpeed;
+    public Vector3 _moveToward;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        this._moveSpeed = Random.Range(this.moveSpeedMin, this.moveSpeedMax);
+        var playerObjList = GameObject.FindGameObjectsWithTag("Player");
+        var targetPlayer = playerObjList[Random.Range(0, playerObjList.Length - 1)];
+        var directionVector = targetPlayer.transform.position - this.transform.position;
+        directionVector.y = 0;
+        var directionUnitVector = directionVector / Vector3.Magnitude(directionVector);
+        this._moveToward = directionUnitVector * 30;
+        this._moveToward.y = 1.5f;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (this.transform.position == this._moveToward)
+        {
+            Destroy(this.gameObject);
+        }
+        this.transform.position = Vector3.MoveTowards(this.transform.position, this._moveToward, this._moveSpeed);
+        this.transform.Rotate(new Vector3(0, this.rotetionSpeed, 0));
+    }
+}
---
+++ Assets/Scripts/ZabutonSpawner.cs
@@ -0,0 +1,33 @@
+﻿using UnityEngine;
+
+public class ZabutonSpawner : MonoBehaviour
+{
+    public GameObject zabutonPrehub;
+    public float spawnSpanMin = 10.0f;
+    public float spawnSpanMax = 30.0f;
+    public float spawnPointRadius = 10.0f;
+
+    private float _nextSpawnSpan;
+    private float _spendTimeAfterPrevSpawn = 0.0f;
+
+    private void Start()
+    {
+        this._nextSpawnSpan = Random.Range(this.spawnSpanMin, this.spawnSpanMax);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        this._spendTimeAfterPrevSpawn += Time.deltaTime;
+
+        if (this._spendTimeAfterPrevSpawn >= this._nextSpawnSpan)
+        {
+            var zabutonObj = GameObject.Instantiate(this.zabutonPrehub);
+            var vector = new Vector3(Random.Range(-1.0f, 1.0f), 0, Random.Range(-1.0f, 1.0f));
+            var unitVector = vector / Vector3.Magnitude(vector);
+            zabutonObj.transform.position = new Vector3(unitVector.x * spawnPointRadius, 1.5f, unitVector.z * spawnPointRadius);
+            this._spendTimeAfterPrevSpawn = 0.0f;
+            this._nextSpawnSpan = Random.Range(this.spawnSpanMin, this.spawnSpanMax);
+        }
+    }
+}
