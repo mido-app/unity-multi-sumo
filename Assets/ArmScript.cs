7f12484324118d5178f9c21598df8760868d251f
chinen <croquette.picker@gmail.com>
2019-12-06 12:16:43 +0900

    突っ張り機能

---
+++ Assets/ArmScript.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ArmScript : MonoBehaviour
+{    
+    public float tuppariPower = 50.0f;
+    private Rigidbody rb;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        // this.rb = this.GetComponent<Rigidbody>();
+    }
+    
+    private void OnCollisionEnter(Collision collision)
+    {
+        Debug.Log("Arm");
+        if (collision.gameObject.tag != "Player") return;
+        var vector = collision.gameObject.transform.position - this.transform.position;
+        var unitVector = vector / Vector3.Magnitude(vector);
+        var speedVector = unitVector * tuppariPower;
+        collision.gameObject.GetComponent<Rigidbody>().AddForce(speedVector, ForceMode.Impulse);
+    }
+}
---
+++ Assets/TuppariScript.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TuppariScript : MonoBehaviour
+{
+    Animator animator;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        animator = GetComponent<Animator>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(Input.GetKeyDown(KeyCode.A)) {
+			animator.SetTrigger("TuppariTrigger");
+		}
+    }
+}
