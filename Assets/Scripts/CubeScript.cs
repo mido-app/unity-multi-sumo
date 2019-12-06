f8987f0bdb1db0a8d042627f9a3825918c134f17
ULDOMAIN\yutaka.omido <yutaka.omido@ulsystems.co.jp>
2019-12-06 14:09:49 +0900

    zabuton sync fix

--- Assets/Scripts/CubeScript.cs
+++ Assets/Scripts/CubeScript.cs
@@ -98,7 +98,7 @@ public class CubeScript : MonoBehaviourPunCallbacks
         if (other.tag == "Zabuton" && this.deathBlowStatus == DEATHBLOW_NO_POWER)
         {
             this.deathBlowStatus = DEATHBLOW_CAN_USE;
-            Destroy(other.gameObject);
+            PhotonNetwork.Destroy(other.gameObject);
         }
     }
 
