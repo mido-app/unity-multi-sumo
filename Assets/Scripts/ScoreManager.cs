e092f8200ab028d1451edd03e172858c63399f50
chinen <croquette.picker@gmail.com>
2019-12-22 12:45:27 +0900

    スコア表微修正

--- Assets/Scripts/ScoreManager.cs
+++ Assets/Scripts/ScoreManager.cs
@@ -24,9 +24,11 @@ public class ScoreManager : MonoBehaviourPunCallbacks
         if (PhotonNetwork.InRoom)
         {   
             // Roomにいる全プレイヤーに対して以下を実施
-            // プレイヤーのUserIDを取得 → UserIDに紐づけたfallCountを取得 → 画面右上に表示
+            // プレイヤーのUserIDを取得 → UserIDに紐づけたfallCountを取得 → スコア表に表示
             StringBuilder displaySb = new StringBuilder();
-            foreach (var p in PhotonNetwork.PlayerList){
+            // 自分のプレイヤー情報をスコア表の一番上に表示
+            displaySb.Append(PhotonNetwork.NickName + " Fall Count : " + customRoomProperties[PhotonNetwork.LocalPlayer.UserId] + "\n");
+            foreach (var p in PhotonNetwork.PlayerListOthers){
                 displaySb.Append(p.NickName + " Fall Count : " + customRoomProperties[p.UserId] + "\n");
             }
             Text fallText = fallObject.GetComponent<Text>();
